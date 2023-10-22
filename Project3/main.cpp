bc9e67f8297dcc5c872eab8feff908da00c2d5dc
DimamonDESKTOP <dimamon02@gmail.com>
2023-10-22 10:40:07 +0300

    done

---
+++ Project3/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	cout << "Enter number: ";
+	cin >> num;
+	if (num > 0) {
+		cout << "Number is positive" << endl;
+	}
+	else if (num < 0) {
+		cout << "Number is negative" << endl;
+	}
+	else {
+		cout << "Number is zero" << endl;
+	}
+}
